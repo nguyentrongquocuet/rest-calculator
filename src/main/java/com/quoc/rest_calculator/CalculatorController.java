c9a85fec75e14578f621f2e87f62bdec456dab28
quoc187 <nguyentrongquocbk@gmail.com>
2022-03-29 15:29:20 +0700

    feat: really stable

---
+++ src/main/java/com/quoc/rest_calculator/CalculatorController.java
@@ -0,0 +1,24 @@
+package com.quoc.rest_calculator;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class CalculatorController {
+
+    @GetMapping("/calculator")
+    public OperationResult calculate(@RequestParam(value = "input1") String input1, @RequestParam(value = "operation") String operation, @RequestParam(value = "input2") String input2) {
+        String sResult;
+       try {
+           double result = CalculatorService.performOperation(input1, operation, input2);
+           sResult = String.valueOf(result);
+       } catch (Error e) {
+           sResult = e.getMessage();
+       }
+
+       return new OperationResult(sResult);
+    }
+}
---
+++ src/main/java/com/quoc/rest_calculator/OperationResult.java
@@ -0,0 +1,13 @@
+package com.quoc.rest_calculator;
+
+public class OperationResult {
+    private final String result;
+
+    public OperationResult(String v) {
+        this.result = v;
+    }
+
+    public String getResult() {
+        return this.result;
+    }
+}
